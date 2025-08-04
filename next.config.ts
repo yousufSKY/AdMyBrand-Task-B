23163315850acc6e862ef1b01a3d756fe5a8ec96
yousufSKY <skyousuf22@outlook.com>
2025-08-04 09:55:42 +0530

    fixes

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'admybrand.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+      },
     ],
   },
   async headers() {
